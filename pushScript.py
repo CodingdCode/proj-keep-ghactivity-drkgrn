1210075ad41dc16c809a3f978ce41427e3c3c0a0
codindcode <carrascoc81@gmail.com>
2020-04-11 02:39:08 +0000

    pi push

--- pushScript.py
+++ pushScript.py
@@ -5,3 +5,5 @@ cmd.run("git add .",check=True, shell=True)
 cmd.run("git commit -m'pi push'", check=True, shell=True)
 
 cmd.run("git push origin master", check=True, shell=True)
+
+#testing pushing with configed credentials
