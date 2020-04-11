2f46373d2697f8ff8d3f4afec9a74479e418ea40
codindcode <carrascoc81@gmail.com>
2020-04-11 02:31:22 +0000

    pi push

---
+++ pushScript.py
@@ -0,0 +1,7 @@
+import subprocess as cmd
+
+cmd.run("git add .",check=True, shell=True)
+
+cmd.run("git commit -m'pi push'", check=True, shell=True)
+
+cmd.run("git push origin master", check=True, shell=True)
